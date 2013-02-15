454166152580bdc94cde70a0d1062ac008c465f2
Tony Huang <tony.huang.jun@gmail.com>
2013-02-15 11:42:26 -0500

    Added TreeCursor class

---
+++ src/com/example/leafid/TreeCursor.java
@@ -0,0 +1,10 @@
+package com.example.leafid;
+
+public class TreeCursor {
+    String resultID;
+    int databaseIndex;
+    public TreeCursor(String _resultID, int _databaseIndex){
+        resultID = _resultID;
+        databaseIndex = _databaseIndex;
+    }
+}
